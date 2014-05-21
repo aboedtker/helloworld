af2def97d7b1d4b051768cca7fb11b22a845f483
aboedtker <asejerst@bates.edu>
2014-05-21 15:37:46 -0400

    restyled fixed error

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,6 +1,21 @@
 module OrdersHelper
 def description_error
-    if @order.errors[:description].size > 0
+    if @order.errors[:First_Name].size > 0
+      return "has-error"
+    end
+     if @order.errors[:Last_Name].size > 0
+      return "has-error"
+    end
+     if @order.errors[:Location].size > 0
+      return "has-error"
+    end
+     if @order.errors[:Time].size > 0
+      return "has-error"
+    end
+     if @order.errors[:Cuisine].size > 0
+      return "has-error"
+    end
+     if @order.errors[:Email].size > 0
       return "has-error"
     end
 
